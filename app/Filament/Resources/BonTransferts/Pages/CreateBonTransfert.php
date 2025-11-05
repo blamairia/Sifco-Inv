8864609638eda069979f17bbba27539c223522a1
Billel Lamairia <blamairia@gmail.com>
2025-11-05 09:02:44 +0100

    debug: Added logging to CreateBonTransfert to diagnose items not saving issue

--- app/Filament/Resources/BonTransferts/Pages/CreateBonTransfert.php
+++ app/Filament/Resources/BonTransferts/Pages/CreateBonTransfert.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\BonTransferts\Pages;
 
 use App\Filament\Resources\BonTransferts\BonTransfertResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Log;
 
 class CreateBonTransfert extends CreateRecord
 {
@@ -19,6 +20,27 @@ class CreateBonTransfert extends CreateRecord
         // Set the requesting user
         $data['requested_by_id'] = auth()->id() ?? 1;
         
+        // Log the data to see what's being passed
+        Log::info('CreateBonTransfert - Data before create:', [
+            'has_rollItems' => isset($data['rollItems']),
+            'rollItems_count' => isset($data['rollItems']) ? count($data['rollItems']) : 0,
+            'has_productItems' => isset($data['productItems']),
+            'productItems_count' => isset($data['productItems']) ? count($data['productItems']) : 0,
+            'has_bonTransfertItems' => isset($data['bonTransfertItems']),
+            'bonTransfertItems_count' => isset($data['bonTransfertItems']) ? count($data['bonTransfertItems']) : 0,
+        ]);
+        
         return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        // Log after create to verify items were saved
+        $this->record->refresh();
+        Log::info('CreateBonTransfert - After create:', [
+            'bon_id' => $this->record->id,
+            'bon_number' => $this->record->bon_number,
+            'items_count' => $this->record->bonTransfertItems()->count(),
+        ]);
     }
 }
